a38369fa97a8c8a18e5a863159b7c9e1287829ce
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-03-03 02:08:02 +0530

    Added logic to stop previous audio

--- script.js
+++ script.js
@@ -15,9 +15,20 @@ sounds.forEach((sound) => {
 
   // Adds a click listener to play
   btn.addEventListener('click', () => {
+    // Stops all existing sounds
+    stopAudio();
+    // Play the selected audio
     document.getElementById(sound).play();
   });
 
   // Adds button for every udio element
   document.getElementById('buttons').appendChild(btn);
 });
+
+function stopAudio() {
+  sounds.forEach((sound) => {
+    const audio = document.getElementById(sound);
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
