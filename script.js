490100c869361ebf572de5606fcc8fd25a3106db
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-03-03 02:02:28 +0530

    Added comments

--- script.js
+++ script.js
@@ -8,12 +8,16 @@ const sounds = [
 
 sounds.forEach((sound) => {
   const btn = document.createElement('button');
+
+  // Adds class 'btn' to every audio element with Text secifie in array
   btn.classList.add('btn');
   btn.innerText = sound;
 
+  // Adds a click listener to play
   btn.addEventListener('click', () => {
     document.getElementById(sound).play();
   });
 
+  // Adds button for every udio element
   document.getElementById('buttons').appendChild(btn);
 });
